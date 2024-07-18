419700c1db5f2de5f535c2917e1e15a5505436c7
Javierelix <javierfevidaho@gmail.com>
2024-07-17 21:00:43 -0600

    Add new Koyeb host to ALLOWED_HOSTS

--- bank/settings.py
+++ bank/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,uneven-idalina-cyberlotto-62107c4f.koyeb.app').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,uneven-idalina-cyberlotto-62107c4f.koyeb.app,ltd-brandea-cyberlottobank-7684fb46.koyeb.app').split(',')
 
 # Application definition
 INSTALLED_APPS = [
