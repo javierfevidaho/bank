ccb3f5cfa210fb3b13f7607705170566d883ac36
Javierelix <javierfevidaho@gmail.com>
2024-07-18 20:17:41 -0600

    fixing deposit and admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'balance')
+    search_fields = ('user__username', 'user__email')
+    list_filter = ('user',)
+    ordering = ('user',)
+    fields = ('user', 'balance')
+    readonly_fields = ('user',)
+
+admin.site.register(Account, AccountAdmin)
