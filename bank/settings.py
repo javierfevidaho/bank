cadab60bc6dd375af2bb792b575c139094bacfab
Javierelix <javierfevidaho@gmail.com>
2024-07-23 17:12:51 -0600

    Reparando error

--- bank/settings.py
+++ bank/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
